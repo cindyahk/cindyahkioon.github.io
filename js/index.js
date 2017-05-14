b3a1dbc98ab9e48665d6f5d019f920dc6a198b1c
cindyahkioon <cindyak86@gmail.com>
2017-05-14 17:24:37 +0800

    Fix top menu navigation hovering

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-var url = location.href;
-$.each($(".nav-link"), function() {
-  if(url.indexOf($(this).attr("href")) > 0 ) $(this).attr("class", "nav-link active");
+$(document).ready(function() {
+  var url = location.href;
+  alert(url);
+  $.each($(".nav-link"), function() {
+    if(url.indexOf($(this).attr("href")) > 0 ) $(this).attr("class", "nav-link active");
+  });
 });
