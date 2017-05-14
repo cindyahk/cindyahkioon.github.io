575a7ec6ba5e67841e63080d9614aca5d6834a28
cindyahkioon <cindyak86@gmail.com>
2017-05-14 17:25:21 +0800

    Remove alert -.-

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   var url = location.href;
-  alert(url);
   $.each($(".nav-link"), function() {
     if(url.indexOf($(this).attr("href")) > 0 ) $(this).attr("class", "nav-link active");
   });
