98e629bebd1925cb8c2d9ab21b9b2eee781968a5
cindyahkioon <cindyak86@gmail.com>
2017-05-28 15:11:03 +0800

    fixing some scripts

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<script src='js/jquery.min.js'></script>
+<script src='js/jquery-3.2.1.min.js'></script>
 <script src='https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js'></script>
 <script src='js/bootstrap.min.js'></script>
 <script src="js/index.js"></script>
--- portfolio.php
+++ portfolio.php
@@ -5,13 +5,13 @@
         <h1>PORTFOLIO</h1>
       </div>
       <div class="menu">
-        <a target="_blank" href="#front">home</a> / <a target="_blank" href="#about">about</a> / <a target="_blank" href="#portfolio">portfolio</a> / <a target="_blank" href="#contact">contact</a>
+        <a href="#front">home</a> / <a href="#about">about</a> / <a href="#portfolio">portfolio</a> / <a href="#contact">contact</a>
       </div>
     </div>
     <div id="portfolio-right" class="right col-lg-6 col-md-12">
       <div class="row">
         <div class="col-md-12">
-          <a target="_blank" href="http://codepen.io/cindyak86/full/Mmpwpp" class="project project-weather">
+          <a href="http://codepen.io/cindyak86/full/Mmpwpp" class="project project-weather">
             <img class="project-img" width="100%"/>
             <h4 class="project-title">Weather Page</h4>
           </a>
@@ -19,7 +19,7 @@
       </div>
       <div class="row">
         <div class="col-md-12">
-          <a target="_blank" href="http://codepen.io/cindyak86/full/oWbXvz" class="project project-rquote">
+          <a href="http://codepen.io/cindyak86/full/oWbXvz" class="project project-rquote">
             <img class="project-img" width="100%"/>
             <h4 class="project-title">Random Quote</h4>
           </a>
@@ -27,7 +27,7 @@
       </div>
       <div class="row">
         <div class="col-md-12">
-          <a target="_blank" href="http://codepen.io/cindyak86/full/wdJgEP"  class="project project-wikipedia">
+          <a href="http://codepen.io/cindyak86/full/wdJgEP"  class="project project-wikipedia">
             <img class="project-img"  width="100%"/>
             <h4 class="project-title">Wikipedia Search</h4>
           </a>
